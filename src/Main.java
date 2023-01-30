247505037c4524a8552fdda1571813fe57778d37
TG Frerich <themuppet.coder@gmail.com>
2023-01-30 12:50:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Hans", 1);
+        Student student2 = new Student("Tony", 2);
+        Student student3 = new Student("Lena", 3);
+        Student student4 = new Student("Tom", 4);
+
+
+
+
+    }
+}
---
+++ src/StudentDB.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentDB {
+
+    //private Student[] students; old version
+    private List<Student> studentList = new ArrayList<>();
+
+    public StudentDB(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDB{" +
+                "studentList=" + studentList +
+                '}';
+    }
+//public Student[] getAllStudents() {
+    //    return students;
+    //}
+
+
+
+}
+
+
+
